e02a77dab67306c32d6bcbfdbf65ef2ece7406a4
jeon120710 <jeon120710@gmail.com>
2026-08-16 21:12:01 +0900

    기타 수정

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,9 @@
 const { REST, Routes, SlashCommandBuilder, ChannelType } = require('discord.js');
 require('dotenv').config({ path: '.env' });
 
-const clientId = process.env.CLIENT_ID || '1516064597638123730';
 const guildId = '1525458537139146812';
 
 const globalCommands = [
-  new SlashCommandBuilder()
-    .setName('등급역할')
-    .setDescription('모든 유저의 등급에 맞춰 역할을 부여합니다.'),
   new SlashCommandBuilder()
     .setName('입장채널')
     .setDescription('입장 로깅 채널을 전역으로 설정합니다.')
@@ -22,15 +18,8 @@ const globalCommands = [
 
 const guildCommands = [
   new SlashCommandBuilder()
-    .setName('랭킹채널')
-    .setDescription('선택한 채널에 후원금액 랭킹을 게시합니다.')
-    .addChannelOption((option) =>
-      option
-        .setName('채널')
-        .setDescription('랭킹을 게시할 텍스트 채널')
-        .addChannelTypes(ChannelType.GuildText)
-        .setRequired(true)
-    ),
+    .setName('등급역할')
+    .setDescription('모든 유저의 등급에 맞춰 역할을 부여합니다.'),
   new SlashCommandBuilder()
     .setName('퇴장채널')
     .setDescription('퇴장 로깅 채널을 설정합니다.')
@@ -38,6 +27,16 @@ const guildCommands = [
       option
         .setName('채널')
         .setDescription('퇴장 메시지를 보낼 텍스트 채널')
+        .addChannelTypes(ChannelType.GuildText)
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName('랭킹채널')
+    .setDescription('선택한 채널에 후원금액 랭킹을 게시합니다.')
+    .addChannelOption((option) =>
+      option
+        .setName('채널')
+        .setDescription('랭킹을 게시할 텍스트 채널')
         .addChannelTypes(ChannelType.GuildText)
         .setRequired(true)
     ),
@@ -67,9 +66,14 @@ const guildCommands = [
 ].map((command) => command.toJSON());
 
 async function main() {
-  if (!process.env.DISCORD_TOKEN) throw new Error('DISCORD_TOKEN이 설정되어 있지 않습니다.');
+  if (!process.env.DISCORD_TOKEN) {
+    throw new Error('DISCORD_TOKEN이 설정되어 있지 않습니다.');
+  }
 
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+  const application = await rest.get(Routes.oauth2CurrentApplication());
+  const clientId = application.id;
+
   await rest.put(Routes.applicationCommands(clientId), { body: globalCommands });
   await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: guildCommands });
   console.log('슬래시 커맨드 배포가 완료되었습니다.');
